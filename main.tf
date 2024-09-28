2c8a4c704aa863bee9be29ad97216c7c8f8f1755
Sparsh Khandelwal <sparshk848@protonmail.com>
2024-09-28 22:21:39 +0530

    Initial commit

--- main.tf
+++ main.tf
@@ -50,6 +50,13 @@ resource "aws_instance" "ec2-instance" {
   user_data = <<-EOF
         #!/bin/bash
         curl -sfL https://get.k3s.io | sh -
+        sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+        echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+        https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+        /etc/apt/sources.list.d/jenkins.list > /dev/null
+        sudo apt-get update -y
+        sudo apt-get install fontconfig openjdk-17-jre -y
+        sudo apt-get install jenkins -y
         EOF
 
   tags = {
